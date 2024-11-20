db2a87f462979e48f27bdc2c4b41d1a63c370abf
Asunatan <105210894+Asunatan@users.noreply.github.com>
2024-11-20 14:44:07 +0800

    Update modules.py

--- core/modules.py
+++ core/modules.py
@@ -1019,7 +1019,7 @@ class CAM_Module(nn.Module):
 
 
 class SE(nn.Module):
-    def __init__(self,input_channels,reduction=2):
+    def __init__(self,input_channels,reduction=4):
         super(SE,self).__init__()
         self.avgpool = nn.AdaptiveAvgPool2d(1)
         self.fc1 = nn.Conv2d(input_channels, input_channels//reduction, 1)
